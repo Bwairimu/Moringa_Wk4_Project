fa9f8d8537dae16fbf63f2f61566d787b217a298
Bah Wairimu <bah.irimu@gmail.com>
2022-01-19 00:37:10 +0300

    added containers

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
-var myCarousel = document.querySelector('#myCarousel')
-var carousel = new bootstrap.Carousel(myCarousel)
+
+var carouselGallery = document.querySelector('#carouselGallery')
+var carousel = new bootstrap.carousel(carouselGallery, {
+  interval: 2000,
+  wrap: false
+})
+var carouselGallery = document.getElementById('carouselGallery')
+carouselGallery.addEventListener('slide.bs.carousel', function () {
+  // do something...
+})
+
+
