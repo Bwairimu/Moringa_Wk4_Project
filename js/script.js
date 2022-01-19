90b98363c52d3afc779eefc8b2fb8b0c7623b6c2
Bah Wairimu <bah.irimu@gmail.com>
2022-01-19 17:12:51 +0300

    Added more columns and images

--- js/script.js
+++ js/script.js
@@ -1,12 +1,26 @@
 
-var carouselGallery = document.querySelector('#carouselGallery')
-var carousel = new bootstrap.carousel(carouselGallery, {
-  interval: 2000,
-  wrap: false
-})
-var carouselGallery = document.getElementById('carouselGallery')
-carouselGallery.addEventListener('slide.bs.carousel', function () {
-  // do something...
-})
-
+$(document).ready(function(){
+  // Activate Carousel
+  $("#carouselGallery").carousel();
+  // Enable Carousel Indicators
+  $(".carousel-item1").click(function(){
+    $("#carouselGallery").carousel(0);
+  });
+  $(".carousel-item2").click(function(){
+    $("#carouselGallery").carousel(1);
+  });
+  $(".carousel-item3").click(function(){
+    $("#carouselGallery").carousel(2);
+  });
+  $(".carousel-item4").click(function(){
+    $("#carouselGallery").carousel(3);
+  });
+  // Enable Carousel Controls
+  $(".left").click(function(){
+    $("#carouselGallery").carousel("prev");
+  });
+  $(".right").click(function(){
+    $("#carouselGallery").carousel("next");
+  });
+});
 
