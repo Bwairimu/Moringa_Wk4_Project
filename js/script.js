d03218477ab21cbaf03cb3e210dce467bbc542ff
Bah Wairimu <bah.irimu@gmail.com>
2022-01-20 08:36:59 +0300

    Added js scripts

--- js/script.js
+++ js/script.js
@@ -23,3 +23,46 @@ $(document).ready(function(){
     $("#carouselGallery").carousel("next");
   });
 });
+
+function pizza(toppings, size){
+  this.toppings =toppings,
+  this. size=size
+}
+pizza.prototype.toppingsBasedAmount=function(){
+  if(this.toppings.length!=0){
+    var toppingsPrice=this.toppings.length*3;
+    return toppingsPrice;
+  }else{
+    alert("Please choose your Toppings")
+  }
+}
+pizza.prototype.sizeBasedAMount=function(){
+  if(this.size==="small"){
+    return 500;
+  }else if (this.size==="Medium"){
+    return 800;
+  }else if (this.size==="Large"){
+    return 1100;
+  }else {
+    alert ("Please select your preferred size")
+  }
+}
+pizza.prototype.calculateCost=function(toppingsBasedAmount,sizeBasedAMount){
+  var totalCost=toppingsBasedAmount + sizeBasedAMount;
+  $("#displayTotalCost").html(totalCost);
+}
+var pizza;
+$(document).ready(function(){
+  $("#onlineForm").submit(function(event){
+    event.prventDefault();
+    $("#show-Bill").show();
+    var inputtedName=$("#name").val();
+    var inputtednumToppings=parseInt($("#numToppings").val());
+    var toppingsArray=[];
+    $("input:checkbox[name=toppingsCheck]:checked").each(function(){
+      var inputtedToppings =$(this).val();
+      toppingsArray.push(inputtedToppings);
+      $("#displayToppings").text(inputtedToppings + ",");
+    });
+  });
+});
